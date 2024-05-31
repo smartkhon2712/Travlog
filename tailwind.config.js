b0c8f8260f7ace5595c015e2fc4fd11be87e6a4e
smartkhon2712 <smartkhon2712@gmail.com>
2024-05-31 23:41:44 +0800

    Travlog

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    colors: {
+      "dim-light": "#EEEEEE",
+      "light-dark": "#222831",
+      white: "#FFFFFF",
+      black: "#000000",
+      dark: "#191825",
+      purple: "#5D50C6",
+      pink: "#F85E9F",
+      borderline: "rgba(25, 24, 37, 0.1)",
+      silver: "rgba(25, 24, 37, 0.5)",
+      grey: "rgba(25, 24, 37, 0.75)",
+      orange: "#FF5722",
+      "light-yellow": "rgba(250, 205, 73, 0.08)",
+    },
+  },
+};
